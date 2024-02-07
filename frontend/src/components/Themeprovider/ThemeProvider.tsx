a0bc6b0a9a39e1df3b26d84210808b23ebc34e4b
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-02-06 21:31:52 -0500

    Added: Db sanity installation

---
+++ frontend/src/components/Themeprovider/ThemeProvider.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+import ThemeContext from "@/context/themeContext";
+
+/**
+ * Componente que proporciona un tema oscuro o claro a la aplicación.
+ * 
+ * @param children Los elementos hijos que se renderizarán dentro del componente.
+ * @returns El componente ThemeProvider.
+ */
+const ThemeProvider = ({ children} : { children: React.ReactNode }) => {
+    const themeFromLocalStorage : boolean = typeof localStorage !== undefined && localStorage.getItem("hotel-theme") ? JSON.parse(localStorage.getItem("hotel-theme")  ! ) : false;
+
+    const [ darkTheme, setDarkTheme ] = useState<boolean>(themeFromLocalStorage);
+    const [renderComponent, setRenderComponent] = useState(false);
+
+    useEffect(() => {
+        setRenderComponent(true);
+    }, [])
+
+    if (!renderComponent) return <></>;
+
+    return (
+        <ThemeContext.Provider value={{darkTheme, setDarkTheme}}>
+            <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+                <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+                    {children}
+                </div>
+            </div>
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
