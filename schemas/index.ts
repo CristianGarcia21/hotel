65f021708dd40cc71eb8594086af892d9b4b3a4e
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-02-11 18:25:53 -0500

    Added: Schemas for app

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,12 @@
+/**
+ * Schema types for hotel management.
+ * @module schemas
+ */
 import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import review from "./review";
 import user from "./user";
+import verificationToken from "./verificationToken";
 
-export const schemaTypes = [user, account]
+export const schemaTypes = [user, account, booking, hotelRoom, review, verificationToken]
