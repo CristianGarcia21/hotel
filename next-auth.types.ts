d70c4c3d2e6fb5faaccc13ea158198604eece540
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-02-14 23:43:23 -0500

    Added: Hero section Component

--- next-auth.types.ts
+++ next-auth.types.ts
@@ -1,3 +1,7 @@
+/**
+ * Extends the Session interface from the "next-auth" module.
+ * Adds a "user" property to the Session interface with an "id" field.
+ */
 import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
