efb0b7e4cce556995bc3473883405ec7b08b8568
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-02-18 23:56:34 -0500

    Added: Component newsLetter and featuredRoom

--- src/components/HeroSection/ClientComponent.tsx
+++ src/components/HeroSection/ClientComponent.tsx
@@ -19,15 +19,15 @@ const ClientComponent: FC<Props> = (props) => {
         <div className="flex justify-between mt-12">
           <div className="flex gap-3 flex-col items-center justify-center">
             <p className=" text-xs lg:text-xl text-center">Basic Room</p>
-            <CountUpNumber endValue={50} duration={2000} />
+            <CountUpNumber endValue={50} duration={1000} />
           </div>
           <div className="flex gap-3 flex-col items-center justify-center">
             <p className=" text-xs lg:text-xl text-center">Luxury Room</p>
-            <CountUpNumber endValue={120} duration={2000} />
+            <CountUpNumber endValue={120} duration={2500} />
           </div>
           <div className="flex gap-3 flex-col items-center justify-center">
             <p className=" text-xs lg:text-xl text-center">Suite</p>
-            <CountUpNumber endValue={60} duration={2000} />
+            <CountUpNumber endValue={60} duration={1900} />
           </div>
         </div>
       </div>
